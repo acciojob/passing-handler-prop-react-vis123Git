27cfdbded79c97469bb9015e42c20c91106bb733
VishalKumarSain <vishaldhoria007@gmail.com>
2024-03-29 21:48:12 +0530

    first

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import "../styles/Child.css";
+const Selection = (props) => {
+  console.log("props===", props);
+  // const {key} = props
+  return (
+    <>
+      <div className="fix-box"></div>
+    </>
+  );
+};
+export default Selection;
+
+
+
 
