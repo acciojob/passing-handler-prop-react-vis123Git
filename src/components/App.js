79ed007e0b3931dd285c59d4593693a91daf7817
VishalKumarSain <vishaldhoria007@gmail.com>
2024-03-31 17:33:35 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
-import '../styles/App.css';
-import Selection from './Selection';
-import ColourSelector from './ColourSelector';
+import "../styles/App.css";
+import Selection from "./Selection";
+import ColourSelector from "./ColourSelector";
 
-const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
-]
+const colourConfig = [
+  {
+    key: "blue",
+    label: "Blue",
+    classname: "btn-blue",
+    background: "rgb(34, 193, 195)",
+  },
+  {
+    key: "orange",
+    label: "Orange",
+    classname: "btn-orange",
+    background: "rgb(221, 112, 18)",
+  },
+  {
+    key: "green",
+    label: "Green",
+    classname: "btn-green",
+    background: "rgb(44, 209, 88)",
+  },
+];
 
-const title = 'Select the gradient and then the Box to change the color';
+const title = "Select the gradient and then the Box to change the color";
 
 const App = () => {
-  let [nextBackground, selectNextBackground] = useState({ background: "" })
+  let [nextBackground, selectNextBackground] = useState({ background: "" });
   const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground)
-  }
-
+    updateSelectionStyle(nextBackground);
+  };
   return (
     <div id="master">
       <h5 className="heading">{title}</h5>
@@ -39,16 +41,13 @@ const App = () => {
         ))}
       </div>
 
-      <div className='row' id="children-wrapper">
-        {
-          ["selection1", "selection2", "selection3"].map(key => (
-            <Selection key={key} applyColor={applyColor} />
-          ))
-        }
+      <div className="row" id="children-wrapper">
+        {["selection1", "selection2", "selection3"].map((key) => (
+          <Selection key={key} applyColor={applyColor} styles={{ background: nextBackground.background }} onClick={() => applyColor(background)} />
+        ))}
       </div>
-    </div >
-  )
-}
-
+    </div>
+  );
+};
 
 export default App;
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from "react";
 import "../styles/Child.css";
-const Selection = (props) => {
-  console.log("props===", props);
-  // const {key} = props
+const Selection = ({ applyColor, styles }) => {
+  const [background, setBackground] = useState(styles.background);
+  const handleBoxClick = () => {
+    applyColor(setBackground);
+  };
   return (
     <>
-      <div className="fix-box"></div>
+      <div className="fix-box" style={{ background: background.background }} onClick={handleBoxClick}>
+        Selection
+      </div>
     </>
   );
 };
 export default Selection;
-
-
-
-
